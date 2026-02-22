18e14de6202f10d2c652ea40e00b9d92a3cdc483
Ben <blearn5241@gmail.com>
2026-02-22 13:09:22 -0500

    fixing export

--- node-scripts/ensurePackageIdsInPackageAliases.js
+++ node-scripts/ensurePackageIdsInPackageAliases.js
@@ -5,7 +5,7 @@ import child_process from 'node:child_process';
 const exec = promisify(child_process.exec);
 import { HUB_ALIAS, PACKAGE_ALIASES, PACKAGE_DIRECTORIES } from './constants.js';
 
-export default async function ensurePackageIdsInPackageAliases() {
+export async function ensurePackageIdsInPackageAliases() {
     let packagesToQuery = [];
     for(let packageDirectory of PACKAGE_DIRECTORIES) {
         if(!Object.keys(PACKAGE_ALIASES).includes(packageDirectory.package) && packageDirectory.package) {
