d9d51746972b7d87061b5c0cefb7bf46e324ffd9
Ben <blearn5241@gmail.com>
2026-03-15 17:37:20 -0400

    #370 updated design of recommended article pills

--- LWCUtils/lwc/pilledTile/pilledTile.js
+++ LWCUtils/lwc/pilledTile/pilledTile.js
@@ -3,4 +3,10 @@ import { LightningElement, api } from 'lwc';
 
 export default class PilledTile extends LightningElement {
     @api pills;
+
+    get iconName() {
+        return this.pills?.filter(p => p.name === 'Blog Post').length > 0 ?
+            'utility:notebook' : this.pills?.filter(p => p.name === 'YouTube Playlist').length > 0 ?
+            'utility:video' : 'utility:page';
+    }
 }
